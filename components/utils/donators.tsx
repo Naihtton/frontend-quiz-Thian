8b4af97230ea90913f211d4561b3bf3333585af1
Thian <whitediary1@outlook.com>
2023-09-04 18:57:38 +0700

    fixed donators UI

--- components/utils/donators.tsx
+++ components/utils/donators.tsx
@@ -14,11 +14,8 @@ const Donators: React.FC<Props> = ({ donation }) => {
     const fetchData = async () => {
       try {
         const response = await fetch(API_URL);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data: Donation[] = await response.json();
-        setDonationData(data);
+        if (!response.ok) throw new Error("Network response was not ok");
+        setDonationData(await response.json());
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -26,29 +23,14 @@ const Donators: React.FC<Props> = ({ donation }) => {
 
     fetchData();
 
-        // Set up an interval to fetch data every 2 seconds
-        const intervalId = setInterval(() => {
-            fetchData();
-          }, 2000);
-      
-          // Clean up the interval when the component unmounts
-          return () => clearInterval(intervalId);
-          
-  }, []); // Empty dependency array means it runs only once when the component mounts
+    // Set up an interval to fetch data every 2 seconds
+    const intervalId = setInterval(fetchData, 2000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
 
   const totalAmount = donationData.reduce((total, donation) => total + donation.amount, 0);
-
-  const donationItems = donationData.map((donation, index) => (
-    <Paper key={index} shadow="xs" p="md">
-      <Group>
-        <Text>{donation.firstName}</Text>
-        <Text>{donation.lastName}</Text>
-        <Text>{donation.email}</Text>
-        <Text>{donation.amount}</Text>
-        <Text>{donation.time}</Text>
-      </Group>
-    </Paper>
-  ));
 
   return (
     <Card withBorder shadow="xs" bg="gray.3">
@@ -63,7 +45,17 @@ const Donators: React.FC<Props> = ({ donation }) => {
           THB
         </Title>
       </Group>
-      <Stack>{donationItems}</Stack>
+      <Stack>
+        {donationData.map((donation, index) => (
+          <Paper key={index} shadow="xs" p="md">
+            <Text>{donation.firstName}</Text>
+            <Text>{donation.lastName}</Text>
+            <Text>{donation.email}</Text>
+            <Text>{donation.amount}</Text>
+            <Text>{donation.time}</Text>
+          </Paper>
+        ))}
+      </Stack>
     </Card>
   );
 };
